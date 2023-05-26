c29897a9c9f743db7ae7cc577f3d4b8bf9b16f65
sudhakaropsmx <sudhakar@opsmx.io>
2023-05-26 16:46:32 +0530

    OP-20453: Pipeline Level RBAC feature flag

--- front50-web/src/main/java/com/netflix/spinnaker/front50/controllers/PipelineController.java
+++ front50-web/src/main/java/com/netflix/spinnaker/front50/controllers/PipelineController.java
@@ -45,6 +45,7 @@ import java.util.*;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -69,6 +70,9 @@ public class PipelineController {
   private final Optional<FiatService> fiatService;
   private final FiatConfigurationProperties fiatConfigurationProperties;
   private final FiatStatus fiatStatus;
+
+  @Value("${pipeline.rbac:false}")
+  private boolean isPipelineRbac;
 
   public PipelineController(
       PipelineDAO pipelineDAO,
@@ -167,7 +171,9 @@ public class PipelineController {
       @RequestBody Pipeline pipeline,
       @RequestParam(value = "staleCheck", required = false, defaultValue = "false")
           Boolean staleCheck) {
-    if (staleCheck.equals(true)) {
+    log.debug("Pipeline RBAC Config : {}", isPipelineRbac);
+    log.debug("Pipeline staleCheck Config : {}", staleCheck);
+    if (isPipelineRbac && staleCheck.equals(true)) {
       permissionCheck(pipeline);
     }
     validatePipeline(pipeline, staleCheck);
@@ -186,15 +192,17 @@ public class PipelineController {
     Pipeline pl = pipelineDAO.create(pipeline.getId(), pipeline);
 
     Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    if (isPipelineRbac) {
+      log.debug("Pipeline permission sync");
+      syncRoles();
 
-    syncRoles();
-
-    String username = getUsername(auth);
-    log.info("******user name :{}", username);
-    if (username != null
-        && !username.isEmpty()
-        && fiatPermissionEvaluator.hasCachedPermission(username)) {
-      fiatPermissionEvaluator.invalidatePermission(username);
+      String username = getUsername(auth);
+      log.info("******user name :{}", username);
+      if (username != null
+          && !username.isEmpty()
+          && fiatPermissionEvaluator.hasCachedPermission(username)) {
+        fiatPermissionEvaluator.invalidatePermission(username);
+      }
     }
     return pl;
   }
