2d27a74bc185363e63632f1c0e5d3d4d86027e64
Sanjeev Thatiparthi <54054932+sanopsmx@users.noreply.github.com>
2023-08-17 16:44:30 +0530

    Added temp fix for disabling the pipeline triggers. (#21)

--- front50-web/src/main/java/com/netflix/spinnaker/front50/controllers/PipelineController.java
+++ front50-web/src/main/java/com/netflix/spinnaker/front50/controllers/PipelineController.java
@@ -96,8 +96,8 @@ public class PipelineController {
     this.fiatStatus = fiatStatus;
   }
 
-  @PreAuthorize("#restricted ? @fiatPermissionEvaluator.storeWholePermission() : true")
-  @PostFilter("#restricted ? hasPermission(filterObject.name, 'APPLICATION', 'READ') : true")
+  //@PreAuthorize("#restricted ? @fiatPermissionEvaluator.storeWholePermission() : true")
+  //@PostFilter("#restricted ? hasPermission(filterObject.name, 'APPLICATION', 'READ') : true")
   @RequestMapping(value = "", method = RequestMethod.GET)
   public Collection<Pipeline> list(
       @RequestParam(required = false, value = "restricted", defaultValue = "true")
@@ -164,8 +164,8 @@ public class PipelineController {
     return pipelineDAO.findById(id);
   }
 
-  @PreAuthorize(
-      "@fiatPermissionEvaluator.storeWholePermission() and  hasPermission(#pipeline.application, 'APPLICATION', 'WRITE') and  @authorizationSupport.hasRunAsUserPermission(#pipeline)")
+  //@PreAuthorize(
+     // "@fiatPermissionEvaluator.storeWholePermission() and  hasPermission(#pipeline.application, 'APPLICATION', 'WRITE') and  @authorizationSupport.hasRunAsUserPermission(#pipeline)")
   @RequestMapping(value = "", method = RequestMethod.POST)
   public Pipeline save(
       @RequestBody Pipeline pipeline,
